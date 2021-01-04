3c0fd127cc6b5666a05c5d5b0f47be5c65587837
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-01-04 18:00:23 +0800

    update

---
+++ tool/render.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+function Trac() {
+    echo "[TRAC] [$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+function Info() {
+    echo "\033[1;32m[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] $1\033[0m"
+}
+
+function Warn() {
+    echo "\033[1;31m[WARN] [$(date +"%Y-%m-%d %H:%M:%S")] $1\033[0m"
+    return 1
+}
+
+function CheckNotEmpty() {
+    [ -n "$(eval echo "\$$1")" ] || Warn "$1 is empty"
+}
+
+function Render() {
+    in=$1
+    out=$2
+
+    CheckNotEmpty "in"
+    CheckNotEmpty "out"
+
+    eval "cat > \"${out}\" <<EOF
+$(< "${in}")
+EOF"
+}
+
+function Help() {
+    echo "sh render.sh <input> <output>"
+    echo "example"
+    echo "  sh render.sh values.yaml.tpl values.yaml"
+}
+
+function main() {
+    if [ "$1" == "-h" ] || [ -z "$1" ]; then
+        Help
+    fi
+
+    render "$1" "$2"
+}
+
+main "$@"
